34ecdc067c72e205c809a656f834982b316db8a5
Jerry Trieu <jerry.trieu@here.com>
2019-02-21 11:58:12 +0100

    Remove unused cancel-queue in AfterUpdate
    Signed-off-by: Jerry Trieu <jerry.trieu@here.com>

--- src/main/scala/com/advancedtelematic/director/manifest/AfterUpdate.scala
+++ src/main/scala/com/advancedtelematic/director/manifest/AfterUpdate.scala
@@ -1,13 +1,9 @@
 package com.advancedtelematic.director.manifest
 
-import java.time.Instant
-
-import cats.implicits._
 import com.advancedtelematic.director.data.MessageDataType.UpdateStatus
 import com.advancedtelematic.director.data.Messages.UpdateSpec
 import com.advancedtelematic.director.db.{AdminRepositorySupport, DeviceRepositorySupport, DeviceUpdate}
 import com.advancedtelematic.libats.messaging.MessageBusPublisher
-import com.advancedtelematic.libats.messaging_datatype.DataType.InstallationResult
 import com.advancedtelematic.libats.messaging_datatype.Messages.DeviceInstallationReport
 import slick.driver.MySQLDriver.api._
 
@@ -20,8 +16,6 @@ class AfterDeviceManifestUpdate()
     extends AdminRepositorySupport
     with DeviceRepositorySupport {
 
-  val canceledInstallationResult = InstallationResult(false, "CANCELLED_ON_ERROR", "Cancelled update due to previous installation error")
-
   def clearUpdate(report: DeviceInstallationReport): Future[Unit] =
     for {
       _ <- publishReport(report)
@@ -32,19 +26,6 @@ class AfterDeviceManifestUpdate()
     for {
       version <- deviceRepository.getCurrentVersion(report.device)
       lastVersion <- DeviceUpdate.clearTargetsFrom(report.namespace, report.device, version)
-      // TODO: Properly implement multiple updates queued per device.
-      // Currently there can be only one update queued per device,
-      // so the following code should not be run.
-      updatesToCancel <- adminRepository.getUpdatesFromTo(report.namespace, report.device, version, lastVersion)
-      _ <- updatesToCancel.filter { _.correlationId.isDefined }.toList.traverse { case updateTarget =>
-        publishReport(
-          DeviceInstallationReport(
-            report.namespace,
-            report.device,
-            updateTarget.correlationId.get,
-            canceledInstallationResult,
-            Map(), None, Instant.now))
-      }
     } yield ()
 
   private def publishReport(report: DeviceInstallationReport) = {
